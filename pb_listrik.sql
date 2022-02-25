27c0c360a3e9eb61381a4ef04d6caf91ce28d852
Hernando17 <ginkotsu17@gmail.com>
2022-02-25 11:59:58 +0700

    SQL file update

--- pb_listrik.sql
+++ pb_listrik.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 15, 2022 at 03:27 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Host: localhost:3306
+-- Generation Time: Feb 25, 2022 at 04:59 AM
+-- Server version: 5.7.33
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,7 +40,9 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_admin`, `id_level`) VALUES
-(17, 'admin', '$2y$10$DHXwnHEUaDo4VdhkDhJB0OO5FIqV2nLzX58XWFvaU5x5rF2zRPRMm', 'admin', 1);
+(17, 'admin', '$2y$10$DHXwnHEUaDo4VdhkDhJB0OO5FIqV2nLzX58XWFvaU5x5rF2zRPRMm', 'admin', 1),
+(18, 'asd', '$2y$10$2agSV/KN4hOqsvZPvMorqeFf4q1BXvhdRyDDOFI5h.agzH4WgQSvm', 'nando123', 1),
+(19, 'bank', '$2y$10$3PW0fYtL61AR3dfYUARmBOip4Wc0jeX4P7XIbjaaIZC81iIWAyIQC', 'bank', 3);
 
 -- --------------------------------------------------------
 
@@ -77,6 +79,15 @@ CREATE TABLE `pelanggan` (
   `id_level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `username`, `password`, `nomor_kwh`, `nama_pelanggan`, `alamat`, `id_tarif`, `id_level`) VALUES
+(12, 'admin', '$2y$10$Lm8wXAzgQeZEkPBUMsLqC.tPxPdbempeySec8YTgDVh9F/6/wvxTq', '123123', 'pelanggan', 'Batam', 4, 2),
+(13, 'asd', '$2y$10$5uOd40yp1sITU9WPozeUIeqZZn/JZq1dfCTdsyYHGyaxclMtO8Bam', '123', 'asd', 'asdsad', 4, 2),
+(14, 'pelanggan', '$2y$10$n9JCUu2f5UX9q/OEQuOvuuWYo8HKqaJ/0/XuqZP2wAIxMW8Lx81se', '123', 'pelanggan', '123', 5, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -87,12 +98,21 @@ CREATE TABLE `pembayaran` (
   `id_pembayaran` int(11) NOT NULL,
   `id_tagihan` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
-  `tanggal_pembayaran` varchar(255) NOT NULL,
+  `tanggal_pembayaran` date NOT NULL,
   `bulan_bayar` varchar(255) NOT NULL,
   `biaya_admin` varchar(255) NOT NULL,
   `total_bayar` varchar(255) NOT NULL,
   `id_admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_tagihan`, `id_pelanggan`, `tanggal_pembayaran`, `bulan_bayar`, `biaya_admin`, `total_bayar`, `id_admin`) VALUES
+(4, 8, 12, '2022-02-25', 'Januari', '123123', '2', 17),
+(5, 8, 12, '2022-02-26', 'Januari', '2222', '22222', 17),
+(6, 8, 12, '2022-02-27', 'Januari', '123123', '123123', 17);
 
 -- --------------------------------------------------------
 
@@ -108,6 +128,14 @@ CREATE TABLE `penggunaan` (
   `meter_awal` double NOT NULL,
   `meter_akhir` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penggunaan`
+--
+
+INSERT INTO `penggunaan` (`id_penggunaan`, `id_pelanggan`, `bulan`, `tahun`, `meter_awal`, `meter_akhir`) VALUES
+(10, 12, 'Januari', '2021', 1000, 1000),
+(11, 12, 'Februari', '2222', 23132, 123123);
 
 -- --------------------------------------------------------
 
@@ -125,6 +153,13 @@ CREATE TABLE `tagihan` (
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tagihan`
+--
+
+INSERT INTO `tagihan` (`id_tagihan`, `id_penggunaan`, `id_pelanggan`, `bulan`, `tahun`, `jumlah_meter`, `status`) VALUES
+(8, 10, 12, 'Januari', '2021', '12313', 'belum_lunas');
+
 -- --------------------------------------------------------
 
 --
@@ -136,6 +171,14 @@ CREATE TABLE `tarif` (
   `daya` varchar(255) NOT NULL,
   `tarifperkwh` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tarif`
+--
+
+INSERT INTO `tarif` (`id_tarif`, `daya`, `tarifperkwh`) VALUES
+(4, '13123', '213123'),
+(5, '1111', '11111');
 
 --
 -- Indexes for dumped tables
@@ -200,7 +243,7 @@ ALTER TABLE `tarif`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `level`
@@ -212,31 +255,31 @@ ALTER TABLE `level`
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `penggunaan`
 --
 ALTER TABLE `penggunaan`
-  MODIFY `id_penggunaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_penggunaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tagihan`
 --
 ALTER TABLE `tagihan`
-  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tarif`
 --
 ALTER TABLE `tarif`
-  MODIFY `id_tarif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_tarif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
